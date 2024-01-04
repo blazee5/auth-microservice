4654a6b61b5c334629d2bdb1fa734957470d40f9
blazee5 <vladislavkutsenko742@gmail.com>
2024-01-04 23:45:42 +0600

    fix migration and config

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,5 +6,6 @@ type User struct {
 	LastName   string `json:"last_name" db:"last_name"`
 	Email      string `json:"email" db:"email"`
 	Password   string `json:"password" db:"password"`
+	Avatar     string `json:"avatar" db:"avatar"`
 	IsVerified bool   `json:"is_verified" db:"is_verified"`
 }
--- migrations/20231231144559_create_users_table.sql
+++ migrations/20231231144559_create_users_table.sql
@@ -1,12 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users(
-    id         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    first_name VARCHAR(255) NOT NULL,
-    last_name  VARCHAR(255) NOT NULL,
-    email      VARCHAR(255) NOT NULL UNIQUE,
-    password   VARCHAR(255) NOT NULL,
-    is_verified BOOLEAN DEFAULT false
+    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    first_name  VARCHAR(255) NOT NULL,
+    last_name   VARCHAR(255) NOT NULL,
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL,
+    avatar      VARCHAR(255) NOT NULL DEFAULT '',
+    is_verified BOOLEAN          DEFAULT false,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 -- +goose StatementEnd
 
